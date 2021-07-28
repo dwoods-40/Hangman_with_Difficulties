f79fe4967274f6767dbf007875dfc21d8cdeb8f4
dwoods-40 <85512768+dwoods-40@users.noreply.github.com>
2021-07-27 21:23:59 -0400

    Delete HardWords.py

--- HardWords.py
+++
@@ -1,6 +0,0 @@
-hard_list = ['valetudinarian', 'conscientious', 'paraphernalia', 'onomatopoeia', 
-             'connoisseur', 'bureaucracy', 'camaraderie', 'impecunious', 
-             'incontrovertible', 'pterodactyl', 'euphemism', 'clairvoyant', 
-             'sanctimonious', 'rudementary', 'ostentatious', 'insurmountable', 
-             'quintessential', 'presumptuous', 'extraordinary', 'exhilarating', 
-             'serendipity', 'consequential', 'transcendent', 'synonymous' ]
