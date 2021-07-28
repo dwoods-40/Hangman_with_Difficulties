6813bb253171b0bc7c2632ab8ac9057419c88005
dwoods-40 <85512768+dwoods-40@users.noreply.github.com>
2021-07-27 21:24:06 -0400

    Delete EasyWords.py

--- EasyWords.py
+++
@@ -1,7 +0,0 @@
-easy_list = ['family', 'paint', 'school', 'fish', 'dog', 'food', 'bird', 'apple',
-             'tree', 'water', 'star', 'beach', 'moon', 'taco', 'church', 'book', 
-             'happy', 'shirt', 'pizza', 'skate', 'cloud', 'bike', 'circle', 'rug', 
-             'chair', 'cat', 'run', 'kite', 'car', 'desk', 'fork', 'sun', 'rain', 
-             'hair', 'hat', 'nose', 'eye', 'egg', 'feet', 'knee', 'key', 'city', 
-             'sad', 'frog', 'bee', 'snow', 'lamp', 'hand', 'corn', 'square', 
-             'cake', 'day', 'love', 'mom', 'dad', 'aunt', 'bean', 'wind'] 
